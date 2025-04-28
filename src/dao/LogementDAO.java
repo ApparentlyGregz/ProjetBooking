873b80333b17a16f0020374b82625c4a55e05d75
Arfi345 <ethan.arfi@edu.ece.fr>
2025-04-28 20:02:32 +0200

    correction erreur ethan

--- src/dao/LogementDAO.java
+++ src/dao/LogementDAO.java
@@ -11,7 +11,38 @@ public interface LogementDAO {
 
     // Ajoute la méthode rechercherLogements
     List<Logement> rechercherLogements(String ville, int nbPersonnes, int nbChambres);
-//corr
 
 }
+
+import model.Logement;
+import java.util.List;
+public interface LogementDAO {
+    List<Logement> getAllLogementsAvecImages();
+    int ajouterLogement(Logement logement);
+    boolean supprimerLogement(int idLogement);
+    boolean modifierLogement(Logement logement);
+    List<Logement> getLogementsParVille(String ville);
+
+    // Ajoute la méthode rechercherLogements
+    List<Logement> rechercherLogements(String ville, int nbPersonnes, int nbChambres);import model.Logement;
+import java.util.List;
+public interface LogementDAO {
+    List<Logement> getAllLogementsAvecImages();
+    int ajouterLogement(Logement logement);
+    boolean supprimerLogement(int idLogement);
+    boolean modifierLogement(Logement logement);
+    List<Logement> getLogementsParVille(String ville);
+
+    // Ajoute la méthode rechercherLogements
+    List<Logement> rechercherLogements(String ville, int nbPersonnes, int nbChambres);import model.Logement;
+import java.util.List;
+public interface LogementDAO {
+    List<Logement> getAllLogementsAvecImages();
+    int ajouterLogement(Logement logement);
+    boolean supprimerLogement(int idLogement);
+    boolean modifierLogement(Logement logement);
+    List<Logement> getLogementsParVille(String ville);
+
+    // Ajoute la méthode rechercherLogements
+    List<Logement> rechercherLogements(String ville, int nbPersonnes, int nbChambres);
 
