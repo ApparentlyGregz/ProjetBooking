333448b923a1da6a4c4df4d43314febd3635c01e
maxence808 <149396520+maxence808@users.noreply.github.com>
2025-04-28 22:10:41 +0200

    Update UtilisateurController.java

--- src/controller/UtilisateurController.java
+++ src/controller/UtilisateurController.java
@@ -4,22 +4,22 @@ import dao.UtilisateurDAO;
 import dao.UtilisateurDAOImpl;
 import model.Utilisateur;
 
-// Contrôleur desopérations liées aux utilisateurs
+// controleur desopérations liées aux utilisateurs
 public class UtilisateurController {
     // DAO pour accéder aux données des utilisateurs
     private UtilisateurDAO dao = new UtilisateurDAOImpl();
 
-    // Vérification identifiants de connexion
+    // vérification identifiants de connexion
     public boolean connexion(String identifiant, String mdp) {
         return dao.checkLogin(identifiant, mdp);
     }
 
-    // Inscription nouvel utilisateur dans la base
+    // inscription nouvel utilisateur dans la base
     public boolean inscription(Utilisateur utilisateur) {
         return dao.insert(utilisateur);
     }
 
-    // Récupèration de l'utilisateur à partir de son identifiant
+    // recuperation de l'utilisateur à partir de son identifiant
     public Utilisateur getUtilisateurParIdentifiant(String id) {
         return dao.getByIdentifiant(id);
     }
